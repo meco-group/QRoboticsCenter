c38aea42a1111b3239e9de03c08b7cc18fb3c332
asteinh <armin.steinhauser@kuleuven.be>
2019-10-10 17:13:41 +0200

    fix problem with VS

--- src/QRoboticsCenter/main.cpp
+++ src/QRoboticsCenter/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   a.setApplicationName("QRoboticsCenter");
   a.setOrganizationName("RobSoft");
-  a.setApplicationVersion(VERSION);
+  a.setApplicationVersion(QString(VERSION));
   a.setApplicationDisplayName("QRoboticsCenter");
   a.setWindowIcon(QIcon(":/icons/QRoboticsCenter_logo.png"));
 
